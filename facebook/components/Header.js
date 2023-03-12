3c39a2bb41f292437e91f899d68f1a6e373d22cb
Mastermind <mastermindx33@gmail.com>
2023-03-12 20:48:19 +0530

    Center Component

--- facebook/components/Header.js
+++ facebook/components/Header.js
@@ -13,11 +13,13 @@ import{
     PlayIcon,
     SearchIcon,
     ShoppingCartIcon,
+    UserGroupIcon,
 } from "@heroicons/react/outline";
+import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <div className='flex'>
+    <div className='flex sticky top-0 z-50 bg-white items-center p-2 lg:px-5 shadow-md'>
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -31,10 +33,18 @@ function Header() {
         <SearchIcon className="h-6 text-gray-600" />
         <input 
             className='flex ml-2 items bg-gray-100 outline-none 
-            placeholder-gray-500' type="text" placeholder="Search Facebook"/>
+            placeholder-gray-500 flex-shrink' type="text" placeholder="Search Facebook"/>
       </div>
       {/* Center */}
-      
+        <div className="flex justify-center flex-grow">
+          <div className='flex space-x-6 md:space-x-2'>
+            <HeaderIcon active Icon={HomeIcon}/>
+            <HeaderIcon Icon={FlagIcon}/>
+            <HeaderIcon Icon={PlayIcon}/>
+            <HeaderIcon Icon={ShoppingCartIcon}/>
+            <HeaderIcon Icon={UserGroupIcon}/>
+          </div>
+        </div>
       {/* Right */}
     
     </div>
