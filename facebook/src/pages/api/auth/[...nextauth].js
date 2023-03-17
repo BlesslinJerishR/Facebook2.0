aca14801c01f1d2094a87eef102f5fa444ee7a70
Mastermind <mastermindx33@gmail.com>
2023-03-17 14:26:26 +0530

    Netlify Live

--- facebook/src/pages/api/auth/[...nextauth].js
+++ facebook/src/pages/api/auth/[...nextauth].js
@@ -1,17 +1,16 @@
+// import NextAuth from "next-auth"
+// import Providers from "next-auth/providers"
 
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     Providers.Facebook({
+//     // Add .env.local file + add variables & Developers Facebook Login api
+//       clientId: process.env.FACEBOOK_CLIENT_ID,
+//       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+// }
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    Providers.Facebook({
-    // Add .env.local file + add variables & Developers Facebook Login api 
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
-}
-
-export default NextAuth(authOptions)
+// export default NextAuth(authOptions)
